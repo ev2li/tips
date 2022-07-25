7de142af5e185f3577ed74c6fa0523f8d646dc0b
ev2li <82583122+ev2li@users.noreply.github.com>
2022-07-25 11:52:03 +0800

    Update chanTemplates.js

--- chanTemplates.js
+++ chanTemplates.js
@@ -28,8 +28,36 @@ class WchTemplate extends baseTemplates_1.BaseExpressionTemplate {
     }
 }
 exports.WchTemplate = WchTemplate;
+class CloseTemplate extends baseTemplates_1.BaseExpressionTemplate {
+    constructor() {
+        super();
+    }
+    buildCompletionItem(code, position) {
+        return completionItemBuilder_1.CompletionItemBuilder
+            .create("close", code)
+            .description(`close(expr)`)
+            .replace('close({{expr}})', position, true)
+            .build();
+    }
+}
+exports.CloseTemplate = CloseTemplate;
+class CapTemplate extends baseTemplates_1.BaseExpressionTemplate {
+    constructor() {
+        super();
+    }
+    buildCompletionItem(code, position) {
+        return completionItemBuilder_1.CompletionItemBuilder
+            .create("cap", code)
+            .description(`cap(expr)`)
+            .replace('cap({{expr}})', position, true)
+            .build();
+    }
+}
+exports.CapTemplate = CapTemplate;
 exports.build = () => [
     new RchTemplate(),
-    new WchTemplate()
+    new WchTemplate(),
+    new CloseTemplate(),
+    new CapTemplate(),
 ];
 //# sourceMappingURL=varTemplates.js.map
